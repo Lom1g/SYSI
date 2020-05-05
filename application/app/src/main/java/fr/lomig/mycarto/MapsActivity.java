be965edc8f546cfdc05744db017fc9eb65328762
N7Falcon <62935503+N7Falcon@users.noreply.github.com>
2020-05-05 16:51:11 +0200

    add point

--- application/app/src/main/java/fr/lomig/mycarto/MapsActivity.java
+++ application/app/src/main/java/fr/lomig/mycarto/MapsActivity.java
@@ -4,19 +4,25 @@ import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    //Initialize Variable
+    GoogleMap gMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
+        SupportMapFragment mapFragment = (SupportMapFragment)
+                getSupportFragmentManager().findFragmentById(R.id.map);
         if (mapFragment != null) {
             mapFragment.getMapAsync(this);
         }
@@ -24,6 +30,26 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
+
+        gMap = googleMap;
+
+        gMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                //Creating marker
+                MarkerOptions markerOptions = new MarkerOptions();
+                //Set Marker Position
+                markerOptions.position(latLng);
+                //Set Latitude and Longitude on Marker
+                markerOptions.title(latLng.latitude+":"+latLng.longitude);
+                //Clear the previously Click position
+                gMap.clear();
+                //Zoom the Marker
+                gMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
+                //Add Marker on map
+                gMap.addMarker(markerOptions);
+            }
+        });
 
     }
 }
