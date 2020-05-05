f6c5cceb4a563ac7e4c72b8fcd672576ca00815a
N7Falcon <62935503+N7Falcon@users.noreply.github.com>
2020-05-05 17:02:59 +0200

    Update MapsActivity.java

--- application/app/src/main/java/fr/lomig/mycarto/MapsActivity.java
+++ application/app/src/main/java/fr/lomig/mycarto/MapsActivity.java
@@ -23,15 +23,15 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment)
                 getSupportFragmentManager().findFragmentById(R.id.map);
-        //if (mapFragment != null) {
+        if (mapFragment != null) {
             mapFragment.getMapAsync(this);
-        //}
+        }
     }
 
     @Override
-    public void onMapReady(GoogleMap googleMap) {
+    public void onMapReady(GoogleMap map) {
 
-        gMap = googleMap;
+        gMap = map;
 
         gMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
             @Override
