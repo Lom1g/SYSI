e9e8e5a306ef93c08ba8c560e87ca4ca1270404b
N7Falcon <62935503+N7Falcon@users.noreply.github.com>
2020-05-12 17:03:04 +0200

    Update Lieu.java

--- application/app/src/main/java/fr/lomig/mycarto/Lieu.java
+++ application/app/src/main/java/fr/lomig/mycarto/Lieu.java
@@ -7,4 +7,22 @@ public class Lieu {
     private String title;
     private LatLng position;
 
+    public Lieu() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public LatLng getPosition() {
+        return position;
+    }
+
+    public void setPosition(LatLng position) {
+        this.position = position;
+    }
 }
