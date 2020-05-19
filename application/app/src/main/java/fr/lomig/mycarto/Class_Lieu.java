1f35ffcf603c3e69f492bbe40cc1404e2d433374
N7Falcon <62935503+N7Falcon@users.noreply.github.com>
2020-05-19 15:26:16 +0200

    Update Class_Lieu.java

--- application/app/src/main/java/fr/lomig/mycarto/Class_Lieu.java
+++ application/app/src/main/java/fr/lomig/mycarto/Class_Lieu.java
@@ -2,16 +2,19 @@ package fr.lomig.mycarto;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatDialogFragment;
 
 public class Class_Lieu extends AppCompatDialogFragment {
-    private EditText editTextTilte;
+    private EditText editTextTitle;
+    private ClassLieuListener listener;
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
@@ -31,10 +34,27 @@ public class Class_Lieu extends AppCompatDialogFragment {
                 .setPositiveButton("ok", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-
+                        String title = editTextTitle.getText().toString();
                     }
                 });
-        editTextTilte = view.findViewById(R.id.edit_title);
+        editTextTitle = view.findViewById(R.id.edit_title);
         return builder.create();
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        try {
+            listener = (ClassLieuListener) context;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(context.toString() +
+                    "must implement ClassLieuListener");
+        }
+    }
+
+    public interface ClassLieuListener{
+        void applyTexts(String title);
+
+
     }
 }
