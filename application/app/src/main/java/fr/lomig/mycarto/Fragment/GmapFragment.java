6f462f238475d1f632f80291ff0f1bf903e1c838
m7cochar <morgancochard@laposte.net>
2020-05-28 15:07:29 +0200

    rajout des champs suppress et accepted
    rajout des champs en prévision des multiples acceptations des modérateurs requises pour ajouter ou supprimer un lieu

--- application/app/src/main/java/fr/lomig/mycarto/Fragment/GmapFragment.java
+++ application/app/src/main/java/fr/lomig/mycarto/Fragment/GmapFragment.java
@@ -128,6 +128,7 @@ public class GmapFragment extends Fragment implements OnMapReadyCallback {
                         spot.put("category", cate.getText().toString());
                         spot.put("signaled",false);
                         spot.put("proposed",true);
+                        spot.put("accepted","0");
                         spot.put("rating", "0");
 
                         if (title.getText().toString().matches("") || desc.getText().toString().matches("") || cate.getText().toString().matches("")) {
@@ -202,7 +203,10 @@ public class GmapFragment extends Fragment implements OnMapReadyCallback {
                                         infoLieu.getNoButton().setOnClickListener(new View.OnClickListener() {
                                             @Override
                                             public void onClick(View v) {
-                                                db.collection("spots").document(document.getId()).update("signaled",true);
+                                                if (!document.getBoolean("signaled")) {
+                                                    db.collection("spots").document(document.getId()).update("signaled",true);
+                                                    db.collection("spots").document(document.getId()).update("suppress","0");
+                                                }
                                                 infoLieu.dismiss();
                                             }
                                         });
